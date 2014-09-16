1a3231a4ffbf7a2d7a4c5f50092c354a66c7eec7
Elton Broering <eltonbroering@gmail.com>
2014-09-16 20:00:02 -0300

    Algumas alterações no tests/test.cc e banco de imagens com as mão em Libras

--- tests/teste.cc
+++ tests/teste.cc
@@ -2,32 +2,14 @@
 #include <time.h>
 #include "Tratamento_imagem.h"
 
-String caminho = "./";
+String caminho = "./Tratada/";
 time_t timer;
 struct tm * timeinfo;
 
-void salvar_imagem(Mat image){
-	time (&timer);
-	timeinfo = localtime (&timer);
-	imwrite( caminho+asctime(timeinfo)+".jpg", image );
-	cout << "Funcionando" << endl;
-
-}
-
 int main(int argc, char** argv) {
-	Mat image_old = imread(argv[1], 1);
-	double aux;
 	Mat image;
 	image=imread(argv[1],1);		
 	image = Tratamento_imagem::Hist_and_Backproj(image,atoi(argv[2]));
-	salvar_imagem( image );			
+	imwrite( caminho+argv[1], image );		
 
-	while(true)
-			{
-				imshow("C em Libras",image);
-				if(waitKey(30)>=0)
-					break;
-			}
-	
 }
-
