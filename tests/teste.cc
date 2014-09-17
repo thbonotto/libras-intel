fdc8cd2e264eea54942c19024112aee15ae7821c
Elton Ferreira Broering <ebroering@localhost.localdomain>
2014-09-17 00:29:10 -0300

    Utilizando banco de imagens tratadas para comparar com uma imagem de entrada

--- tests/teste.cc
+++ tests/teste.cc
@@ -1,15 +1,22 @@
 #include <unistd.h>
-#include <time.h>
 #include "Tratamento_imagem.h"
+#include <iostream>
 
-String caminho = "./Tratada/";
-time_t timer;
-struct tm * timeinfo;
+
+String caminho = "../img/";
+String alfabeto[27] = ("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P"
+                       ,"Q","R","S","T","U","V","W","X","Y","Z");
 
 int main(int argc, char** argv) {
 	Mat image;
 	image=imread(argv[1],1);		
 	image = Tratamento_imagem::Hist_and_Backproj(image,atoi(argv[2]));
-	imwrite( caminho+argv[1], image );		
+	int i=0;
+	for(i;i<27;i++){
+		cout << "Letra :"+alfabeto[i] + " =  " + Tratamento_imagem::getPSNR(image,imread(caminho+alfabeto[i]+".jpg")) << endl; 
+	}
+	
+	
+	
 
 }
